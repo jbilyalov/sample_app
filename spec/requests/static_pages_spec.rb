2b1bb5ade9a26e22257b81f139e1b953c752f48d
jbilyalov <jbilyalov@turbosquid.com>
2013-11-19 11:44:34 -0600

    Added more tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(page_title) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
     it { should have_content('Sample App') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
@@ -33,8 +39,21 @@ describe "StaticPages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(" | Contact") }
+  end
+
+  it "should have right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title('About Us')
+    click_link "Help"
+    expect(page).to have_title('Help')
+    click_link "Contact"
+    expect(page).to have_title('Contact')
+    click_link "Sign in"
+    expect(page).to have_title('Sign Up')
+    click_link "sample app"
+    expect(page).to have_title('Sample App')
   end
 end
